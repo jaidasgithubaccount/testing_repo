d1f9342f066130a54abd20b4264aa13878130405
jaidasgithubaccount <112672744+jaidasgithubaccount@users.noreply.github.com>
2024-12-02 03:14:49 +0000

    another whole-site update

--- pages/culturewarftp.py
+++ pages/culturewarftp.py
@@ -2,6 +2,35 @@ import streamlit as st
 
 # Show title and description.
 st.title("The Culture War for the Planet")
-st.header('Comparative analysis of r/collapse, r/preppers, r/itcouldhappenhere, and r/collapse')
+st.header('Comparative analysis of r/collapse, r/preppers, r/itcouldhappenhere, and r/climate')
 
+st.subheader("What's going on on with r/Collapse?")
 
+st.markdown("In our worsening climate catastrophe, I've noticed the growth of a particular doomerist subreddit - r/collapse - and, perhaps more notably, the spread of its doomer rhetoric to other, less-controversial climate related subreddits, like r/preppers, r/itcouldhappenhere, and r/climate (*in order from most to least transgressive in their tactics*).")
+
+st.markdown("The r/Collapse subreddit is filled with people deriving a number of narratives about the eventual collapse of modern society. I'm aiming to see what these narratives *are*. finding through-lines and (AI-determined, need a coda about it) similarities between different authors' positions, and being able to map out the rhetorical 'state space' - with a keen eye toward any other information I can glean.")
+
+st.subheader("Data Sources")
+
+st.markdown("1. **The Reddit Comments**")
+st.markdown("I pulled 16,000 of the top comments and text posts since 2018 from r/collapse, r/preppers, r/itcouldhappenhere and r/climate, using [Praw](https://github.com/praw-dev/praw):")
+st.markdown("""
+```MIT License
+Copyright (c) 2016, Bryce Boe
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+```
+            """)
+
+st.markdown("I then compared that data with...")
+
+st.markdown("2. [Twitter Climate Change Sentiment Dataset](https://www.kaggle.com/datasets/edqian/twitter-climate-change-sentiment-dataset/data)")
+st.markdown("I compared data ")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,19 @@
 import streamlit as st
-from openai import OpenAI
 
-
+def Home():
 # Show title and description.
-st.title("Jaida's Publicly-Available Work")
-st.header("Institutional Work")
-st.markdown('''
-    **Surfing the Kali Yuga: Tracking the Alt-Right on Twitter (2023)**
-    *Bowdoin College Digital Commons*
-             
-    [Abstract](https://digitalcommons.bowdoin.edu/honorsprojects/431/) | [Download Link](https://digitalcommons.bowdoin.edu/cgi/viewcontent.cgi?article=1472&context=honorsprojects)        
-            ''')
-st.markdown(
-    '''
-    **How 'Don't Say Gay' Bills Harm our Youth (2022)**
-    *Op-Ed, Bangor Daily News*
+    st.title("Jaida's Publicly-Available Work")
+    st.header("Institutional Work")
+    st.markdown("**Surfing the Kali Yuga: Tracking the Alt-Right on Twitter (2023)**")
+    st.markdown("*Bowdoin College Digital Commons*")
+    st.markdown("[Abstract](https://digitalcommons.bowdoin.edu/honorsprojects/431/) | [Download Link](https://digitalcommons.bowdoin.edu/cgi/viewcontent.cgi?article=1472&context=honorsprojects)")
+    st.markdown("**How 'Don't Say Gay' Bills Harm our Youth (2022)**")
+    st.markdown("*Op-Ed, Bangor Daily News*")
+    st.markdown("[News Link](https://www.bangordailynews.com/2022/07/26/opinion/opinion-contributor/harmful-dont-say-gay-bills-opinion-joam40zk0w/)")
+    st.header("Personal Projects")
+    st.page_link("pages/culturewarftp.py", label="The Culture War for the Planet")
+    st.page_link("pages/logicbot.py", label="LogicBot - Argument-Strength Analyzer")
 
-    [News Link](https://www.bangordailynews.com/2022/07/26/opinion/opinion-contributor/harmful-dont-say-gay-bills-opinion-joam40zk0w/) 
-    '''
-)
-st.header("Personal Projects")
-st.page_link("pages/culturewarftp.py", label="The Culture War for the Planet")
-st.page_link("pages/logicbot.py", label="LogicBot")
-
+pg = st.navigation([st.Page(Home), st.Page("pages/culturewarftp.py"), st.Page("pages/logicbot.py")])
+pg.run()
 
